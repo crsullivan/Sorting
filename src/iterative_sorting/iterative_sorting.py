468a2f270d1d62aa86c163a55d6804d6513a2268
Connor Sullivan <crsullivan@protonmail.com>
2020-03-16 14:13:05 -0700

    3 LOC implementation for iterative sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,15 +1,18 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
+    # For each element in the array, 
     for i in range(0, len(arr) - 1):
-        cur_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
+        # Grab it and compare it to the next
         for j in range(i + 1, len(arr)):
-            if arr[cur_index] > arr[j]:
-                cur_index = j
+            #  Grab it and compare it 
+            # if next value is lesser, grab that value, switch with the original and restart the process there
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]    
         # TO-DO: swap
-        arr[i], arr[cur_index] = arr[cur_index], arr[i]
+        
 
     return arr
 
@@ -17,8 +20,11 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
     for i in range(len(arr)):
+        # For each element in the array
         for j in range(0, len(arr) - 1):
+            # Grab it and compare it to the next, checking to see if the original is of greater value
             if arr[j] > arr[j+1]:
+                # If the original is of greater value, push it towards the end of the array and check the next one again
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
